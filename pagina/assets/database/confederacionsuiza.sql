d90cf3defcd404e07789aa3b3f696b1c63ce85f2
Anchart <valenanchart@gmail.com>
2022-11-20 18:44:04 -0300

    hice cambios en la base de datos, cambios en los nombres de las tablas y los campos. Cambios en Register.php. El tag ya se agrega correctamente y se relaciona con el usuario registrado

--- pagina/assets/database/confederacionsuiza.sql
+++ pagina/assets/database/confederacionsuiza.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-11-2022 a las 01:44:05
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
+-- Tiempo de generación: 20-11-2022 a las 22:41:18
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 7.3.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,10 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `asistencia` (
-  `id` int(10) NOT NULL,
-  `idusuario` int(10) NOT NULL,
+  `id` int(4) NOT NULL,
+  `idusuario` int(4) NOT NULL,
   `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `estado` varchar(20) NOT NULL
+  `estado` enum('presente','ausente','tarde') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -53,8 +53,9 @@ CREATE TABLE `curso` (
 --
 
 CREATE TABLE `cursos` (
-  `id` int(255) NOT NULL,
-  `curso` int(3) NOT NULL
+  `id` int(4) NOT NULL,
+  `year` int(1) NOT NULL,
+  `division` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -64,9 +65,9 @@ CREATE TABLE `cursos` (
 --
 
 CREATE TABLE `rol` (
-  `id` int(10) NOT NULL,
-  `idusuario` int(10) NOT NULL,
-  `idrol` int(10) NOT NULL
+  `id` int(4) NOT NULL,
+  `idusuario` int(4) NOT NULL,
+  `idrol` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -76,21 +77,29 @@ CREATE TABLE `rol` (
 --
 
 CREATE TABLE `roles` (
-  `id` int(11) NOT NULL,
-  `rol` int(11) NOT NULL
+  `id` int(4) NOT NULL,
+  `rol` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tags`
+-- Estructura de tabla para la tabla `tag`
 --
 
-CREATE TABLE `tags` (
-  `id` int(10) NOT NULL,
-  `tag` int(28) NOT NULL,
-  `idusuario` int(10) NOT NULL
+CREATE TABLE `tag` (
+  `id` int(4) NOT NULL,
+  `tag` varchar(14) NOT NULL,
+  `idusuario` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tag`
+--
+
+INSERT INTO `tag` (`id`, `tag`, `idusuario`) VALUES
+(7, '637a9d7e0d30d', 18),
+(8, '637a9df81fc7a', 19);
 
 -- --------------------------------------------------------
 
@@ -99,7 +108,7 @@ CREATE TABLE `tags` (
 --
 
 CREATE TABLE `usuario` (
-  `idusuario` int(255) NOT NULL,
+  `id` int(255) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(20) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -111,16 +120,9 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`idusuario`, `nombre`, `apellido`, `email`, `telefono`, `contrasena`) VALUES
-(1, 'aaaa', 'aaaa', 'aaa@gmail.com', 1111111, '$2y$10$bdNXUUd6/8qwk.vYI29tluXjwdAVz937.M0kOqmSBAGqNRNUIfDq2'),
-(2, 'aaaa', 'aaaaa', 'aaaa@gmail.com', 11111, '$2y$10$kWC8uxhsWxKFhBluW0WXCuuicf3fCoCQ95X5Q5heZ1RABu7CivZm2'),
-(3, 'aaaa', 'aaaaa', 'aaaa', 11111, '$2y$10$oQOV4OrJLERz/pYDibjVHOp1DUwM1y/oIDYgFT6KZRNTFMo12ReES'),
-(4, 'aea', 'aea', 'aea@gmail.com', 123, '$2y$10$.Sm/O2ZNtznyfSoFo9cVbuugF.51E61iiM6kj30RJ5BFgsd2PHFA6'),
-(5, 'aea', 'aeae', 'aeae@gmail.com', 123, '$2y$10$v2uZDnvk3i2ObWkemAAy0O/4bNt3CJ7FtdUDuKxz8R7L76x6kSqZy'),
-(6, 'a', 'a', 'a@gmail.com', 123, '$2y$10$RBabCOiN4oef1a6nN.o9GuXcf7EMqxRvgGNOwm1bwyAWPcapY9yp6'),
-(7, 'aa', 'aa', 'aa@gmail.com', 123, '$2y$10$tCn9N3CVsnSKmZo04njlr.0R9hb2HQ9Vtc55CTmQyV2Uee1Yuuk7.'),
-(8, 'eee', 'eee', 'eee@gmail.com', 123, '$2y$10$j3136bhD5dFgvDdS/AGKS.6ofFnOUq6XTyUrL5rMu9ipC0eYWpHZK'),
-(9, 'eeee', 'eeee', 'eeee@gmail.com', 123, '$2y$10$gezsHM/j267/fvbv49LNjehDMM2AQ3KMSuMGbpBTXlArOdiasQSLy');
+INSERT INTO `usuario` (`id`, `nombre`, `apellido`, `email`, `telefono`, `contrasena`) VALUES
+(18, 'Valentin', 'anchart', 'valen@gmail.com', 123, '$2y$10$rMcTzirUqtklX7CUutbp3uWSl4EOzBkH8xAJfoBERBV8ym1DvrVqm'),
+(19, 'noelia', 'laura', 'noe@gmail.com', 123, '$2y$10$HXIT9hM//47gN9VEtVQuKe4Y7uXtFdYiiKYujcZVaCcZO9mAERUc.');
 
 --
 -- Índices para tablas volcadas
@@ -151,16 +153,16 @@ ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `tags`
+-- Indices de la tabla `tag`
 --
-ALTER TABLE `tags`
+ALTER TABLE `tag`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`idusuario`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -170,31 +172,37 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `asistencia`
 --
 ALTER TABLE `asistencia`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `cursos`
 --
 ALTER TABLE `cursos`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tag`
+--
+ALTER TABLE `tag`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idusuario` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- pagina/register.php
+++ pagina/register.php
@@ -15,6 +15,7 @@ if (isset($_POST['telefono'])) $telefono = $_POST['telefono'];
 $hashcontra = password_hash($contrasena, PASSWORD_DEFAULT);
 $idunico = uniqid();
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $sql = "select * from usuario where email = '$email'";
   $result = $conn->query($sql);
@@ -25,35 +26,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 				<strong  class="text-red-700">Este usuario ya existe</strong>.
         	</div>';
   } else {
-    $sql2 = "select * from usuario where nombre = '$nombre'";
-    $result2 = $conn->query($sql);
-    if ($result2->num_rows > 0) {
-      $msg = '
-			<div class="alert alert-danger">
-				<strong  class="text-red-700">Este nombre ya existe</strong>.
-        	</div>';
-    } else {
       $sql = "insert into usuario (nombre,apellido,email,telefono,contrasena) values ('$nombre','$apellido', '$email','$telefono', '$hashcontra')";
-
+      
       if ($conn->query($sql) === TRUE) {
         $msg = '
 			<div class="alert alert-success">
 				<strong class="text-green-700">El usuario se ha creado correctamente!!.</strong> 
         	</div>';
 
-        $sql = "select idusuario from usuario where email = '$email'";
-
+        $sql = "select * from usuario where email = '$email'";
         $result = $conn->query($sql);
-        $string = strval($result);
-        echo $string;
-
-       
+          while ($row = $result->fetch_assoc()) {
+            $id = $row['id'];
+          }
+        $sql = "insert into tag (tag,idusuario) values ('$idunico','$id')";     
+        if ($conn->query($sql) === TRUE) {
+          $msg = $msg.'
+        <div class="alert alert-success">
+          <strong class="text-green-700">El TAG '.$idunico.' se asigno correctamente al usuario.</strong><br> 
+            </div>';} else {
+              $msg = $msg. '<div class="alert alert-danger">
+          <strong class="text-green-700">Hubo un error al asignar el TAG.</strong> 
+            </div>';}
       }
 
       }
 
     }
-  }
+  
 
 
 ?>
@@ -80,7 +80,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <input name="email" id="" placeholder="Email" class="w-full border border-neutral-400 px-4 py-2 text-xl focus:outline-none text-neutral-700">
       <input type="password" name="contrasena" id="" placeholder="Password" class="w-full border border-neutral-400 px-4 py-2 text-xl focus:outline-none text-neutral-700">
       <input type="tel" name="telefono" id="" placeholder="Telefono" class="w-full border border-neutral-400 px-4 py-2 text-xl focus:outline-none text-neutral-700">
-      <p><?php  echo "Tu tag es: $idunico"; ?></p>
       <input class="bg-rose-700 text-white rounded w-full text-2xl py-3 flex justify-center items-center" type="submit" value="Registrarse" class="boton">
       <a href="login.php" class="text-nuetral-700 text-xl">Ya tenes una cuenta? Inicia sesion</a>
       <?php if (isset($msg)) echo $msg ?>
