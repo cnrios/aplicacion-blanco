292d6fc402e4c73da872be1359ee8da45d15c463
anchart <valenanchart@gmail.com>
2022-11-24 17:24:59 -0300

    mejore el index

--- pagina/index.php
+++ pagina/index.php
@@ -18,9 +18,7 @@ if (isset($_GET['q'])) {
   $search = $_GET['q'];
   $sql = "SELECT * FROM asistencia JOIN usuario WHERE asistencia.idusuario = usuario.id AND usuario.apellido = '$q'";
 }
-
 $resultAsistencias = $conn->query($sql);
-
 ?>
 
 <!DOCTYPE html>
@@ -41,31 +39,55 @@ $resultAsistencias = $conn->query($sql);
   <div class="flex flex-row">
     <?php include './partials/aside.php'; ?>
 
+   
     <div class="bg-rose-900 gap-4 flex flex-col w-full p-4 h-full">
 
-      <?php while ($row = $resultAsistencias->fetch_assoc()) { ?>
-        <div class="p-4 z-10 shadow-2xl bg-rose-700 border gap-4 border-neutral-300 rounded flex jusitfy-center items-center">
-          <iframe class="h-[200px] aspect-video rounded z-50 shadow-lg" src="<?php echo $row['id']; ?>" frameborder="0" allowfullscreen></iframe>
-          <div class="relative  shadow-lg bg-rose-600 rounded w-full h-[200px] p-4 gap-2 flex flex-col">
+<?php while ($row = $resultAsistencias->fetch_assoc()) { ?>
+  <div class="p-4 z-10 shadow-2xl bg-rose-700 border gap-4 border-neutral-300 rounded flex jusitfy-center items-center">
+  <img class="border-4 border-black w-[100px] rounded-full" src="<?php 
+  
+  $actualimg = "./assets/images/usuario.jpg";
+  $acutalid = $row['id'];
 
-            <a href="<?php echo $row['id']; ?>" target="_blank" class="cursor-pointer text-4xl text-white"><?php echo $row['apellido']; ?></a>
+            if(file_exists("./assets/images/$acutalid.jpg")){
+               $actualimg = "./assets/images/$acutalid.jpg";
+            }
 
-            <p class="text-2lx text-white h-12 overflow-hidden"><?php echo $row['nombre']; ?></p>
-            
-      
-            <div class="absolute bottom-0 left-0 flex gap-2 m-3">
+            if(file_exists("./assets/images/$acutalid.jpeg")){
+              $actualimg = "./assets/images/$acutalid.jpeg";
+            }
 
-              <p class="rounded-full px-4 py-1 text-white bg-rose-900 shadow-lg"><?php echo $row['estado']; ?></p>
+            if(file_exists("./assets/images/$acutalid.png")){
+             $actualimg = "./assets/images/$acutalid.png";
+             }
 
-              <p class="rounded-full px-4 py-1 text-white bg-rose-900 shadow-lg"><?php echo $row['fecha']; ?></p>
+  echo  $actualimg; ?>" alt="Alumno">
+ 
+    <div class="relative  shadow-lg bg-rose-600 rounded w-full h-[100px] p-4 gap-2 flex flex-col">
 
-            </div>
-          </div>
-        </div>
-      <?php } ?>
-
+      <table>
+        <tr>    
+            <th>nombre</th>
+            <th>apellido</th>
+            <th>fecha</th>
+           <th>estado</th>
+        </tr>
+        <tr>   
+           <form method="POST" action="preceptor.php" name ="formeditar" >                
+              <td><input type="text" name="nombre" value=<?php echo $row['nombre'];?> readonly></td>
+              <td><input type="text" name="apellido" value=<?php echo $row['apellido'];?> readonly></td>
+              <td><input type="text" name="fecha" value=<?php echo $row['fecha'];?> readonly></td>
+              <td><input type="text" name="estado" value=<?php echo $row['estado'];?> readonly></td>
+        </tr>
+      </table>
     </div>
   </div>
+
+<?php } ?>
+
+</div>
+</div>
+
 
 </body>
 
